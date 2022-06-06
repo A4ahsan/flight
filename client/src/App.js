d5478fe6cd076d11ce20c1083825d9b01a156b37
A4ahsan <a4ahsananis@gmail.com>
2022-06-06 12:52:14 -0700

    site

---
+++ client/src/App.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import BookingFlightPage from './pages/BookingFlightPage';
+import BookingFlights from './pages/BookingFlights';
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import BookingHotel from './pages/BookingHotel';
+import BookingHotelPage from './pages/BookingHotelPage';
+import Faq from './pages/Faq';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import SearchHotels from './pages/SearchHotels';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ChoosePayment from "./pages/ChoosePayment";
+import Paypal from './pages/Paypal';
+import Square from './pages/Square';
+import { useSelector } from "react-redux";
+import { selectUser } from "./redux/userSlice"
+import { selectAlert } from "./redux/alertSlice";
+import SimpleAlert from "./components/Alert";
+import SearchFlight from './pages/SearchFlight';
+import FlightDetail from "./pages/FlightDetail";
+
+
+
+
+function App() {
+  // trave -agency functions
+  const currentUser = useSelector(selectUser);
+  const showAlert = useSelector(selectAlert);
+
+  const RequireAuth = (({ children }) => {
+    return currentUser ? children : <Navigate to="/login" />
+  })
+  const RequireAdmin = (({ children }) => {
+    return currentUser.isAdmin ? children : <Navigate to="/" />
+  })
+  const Login = (({ children }) => {
+    return currentUser ? <Navigate to="/" /> : children
+  });
+  return (
+    <div className="App">
+      {showAlert &&
+        <SimpleAlert />
+      }
+      <Router>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/book-hotel-page" element={<BookingHotel />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/search-flights" element={<SearchFlight />} />
+          <Route path="/search-hotels" element={<SearchHotels />} />
+          <Route path='/flight-booking' element={<FlightDetail />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/flight-checkout" element={
+            <RequireAuth>
+              <BookingFlightPage />
+            </RequireAuth>
+          } />
+          <Route path="/hotel-checkout" element={
+            <RequireAuth>
+              <BookingHotelPage />
+            </RequireAuth>
+          } />
+          <Route path="/login" element={
+            <Login>
+              <SignIn />
+            </Login>
+          } />
+          <Route path="/signup" element={
+            <Login>
+              <SignUp />
+            </Login>
+          } />
+          <Route path="/choose-payment" element={
+            <RequireAuth>
+              <ChoosePayment />
+            </RequireAuth>
+          } />
+          <Route path="/paypal" element={
+            <RequireAuth>
+              <Paypal />
+
+            </RequireAuth>
+          } />
+          <Route path="/square" element={
+            <RequireAuth>
+              <Square />
+
+            </RequireAuth>
+          } />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,38 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'xs': '0px',
+      ...defaultTheme.screens,
+    },
+    scale: {
+      '0': '0',
+      '25': '.25',
+      '50': '.5',
+      '75': '.75',
+      '90': '.9',
+      '95': '.95',
+      '100': '1',
+      '105': '1.05',
+      '110': '1.1',
+      '125': '1.25',
+      '150': '1.5',
+      '200': '2',
+      '500': '5'
+    },
+    extend: {
+      width: {
+        '500': '50rem',
+        '400': '40rem',
+        '300': '30rem',
+        '250': '25rem',
+        '200': '20rem',
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
