9f1d2cce84d50e385fca2fa07de0d7f856821e96
hamza-HL <hamza@testedtalent.com>
2022-06-24 18:21:10 +0500

    images added on footer

--- client/src/carousel/HotelSlider.js
+++ client/src/carousel/HotelSlider.js
@@ -8,7 +8,7 @@ function HotelSlider() {
     <div className="popular">
       <div className="popular_inner">
         <figure>
-          <img src="https://axen-trave-test.herokuapp.com/images/popular01.jpg" alt="" className="img-responsive" />
+          <img src="images/popular01.jpg" alt="" className="img-responsive" />
           <div className="over">
             <div className="v1">Bahamas <span>Our biggest sale</span></div>
             <div className="v2">Lorem ipsum dolor sit amet, concateus.</div>
@@ -20,11 +20,11 @@ function HotelSlider() {
           <div className="txt3 clearfix">
             <div className="left_side">
               <div className="stars1" style={{ display: "flex" }} >
-                <img src="https://axen-trave-test.herokuapp.com/images/star1.png" alt="" />
-                <img src="https://axen-trave-test.herokuapp.com/images/star1.png" alt="" />
-                <img src="https://axen-trave-test.herokuapp.com/images/star1.png" alt="" />
-                <img src="https://axen-trave-test.herokuapp.com/images/star1.png" alt="" />
-                <img src="https://axen-trave-test.herokuapp.com/images/star2.png" alt="" />
+                <img src="images/star1.png" alt="" />
+                <img src="images/star1.png" alt="" />
+                <img src="images/star1.png" alt="" />
+                <img src="images/star1.png" alt="" />
+                <img src="images/star2.png" alt="" />
               </div>
               <div className="nums">- 18 Reviews</div>
             </div>
